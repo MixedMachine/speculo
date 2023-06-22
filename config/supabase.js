1013c3b52002d56d6f5bf47ecaca6183a0da51c6
Michael Marinez <80479622+MixedMachine@users.noreply.github.com>
2023-06-21 21:21:56 -0600

    .

--- config/supabase.js
+++ config/supabase.js
@@ -1,6 +1,1 @@
-export default {
-  credentials: {
-    supabaseUrl: 'https://piaeyluvpxjwuxoptkfe.supabase.co',
-    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpYWV5bHV2cHhqd3V4b3B0a2ZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODY4NDUyODQsImV4cCI6MjAwMjQyMTI4NH0.cfjinqSFEYiL50F9damXTWuIVKVax4cboRGvdwfkNFI'
-  }
-}
+
